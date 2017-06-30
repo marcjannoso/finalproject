07344f28079c4573ebd63d794280e3ba86656796
marcjannoso <janno_87ph@yahoo.com>
2017-06-30 00:15:50 -0400

    first commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,6 +88,14 @@
                   </li>
 
                   <li class="list-group-item">
+                    <a href="{{route('tags')}}"> Tags</a>  
+                  </li>
+
+                  <li class="list-group-item">
+                    <a href="{{route('tag.create')}}">Create Tags</a>  
+                  </li>
+
+                  <li class="list-group-item">
                     <a href="{{route('posts')}}"> All posts</a>  
                   </li>
 
